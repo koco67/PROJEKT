a66a16fa991c523c758ecec48dab163d81171893
Proleteus <gthf013mit>
2024-01-05 12:22:28 +0100

    productpage with dropdown menu added, RabbitMQ examples added

--- frontend/src/main/resources/templates/script.js
+++ frontend/src/main/resources/templates/script.js
@@ -13,3 +13,25 @@ function addToCart() {
     cartCount++;
     document.getElementById('cart-count').innerText = cartCount;
 }
+
+function toggleMenu() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    dropdownMenu.style.display = (dropdownMenu.style.display === "block") ? "none" : "block";
+}
+
+document.addEventListener("click", function(event) {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (event.target.closest(".menu-icon") === null && event.target.closest(".dropdown-content") === null) {
+        dropdownMenu.style.display = "none";
+    }
+});
+
+// Close the dropdown when the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-icon')) {
+        var dropdownMenu = document.getElementById("dropdownMenu");
+        if (dropdownMenu.style.display === "block") {
+            dropdownMenu.style.display = "none";
+        }
+    }
+}
