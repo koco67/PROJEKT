ba222e482086d5e619fbca581fb93ee8642716f3
Dhami <Koco.Dhami@Student.HTW-Berlin.de>
2024-02-04 19:49:44 +0100

    Dockerfile for each service, changed com.swa to de.htw in frontend und gateway

--- frontend/src/main/java/de/htw/frontend/FrontendApplication.java
+++ frontend/src/main/java/de/htw/frontend/FrontendApplication.java
@@ -1,4 +1,4 @@
-package com.swa.frontend;
+package de.htw.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gateway/src/main/java/de/htw/gateway/GatewayApplication.java
+++ gateway/src/main/java/de/htw/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.swa.gateway;
+package de.htw.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
