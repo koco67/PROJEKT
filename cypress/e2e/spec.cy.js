2c13c9f2973bbd79925457a09b61708d5aaf3de4
Proleteus <soenke-foerster@web.de>
2024-02-06 19:22:47 +0100

    fixed URL for cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('frontend test', () => {
   it('index page to product pag', () => {
-    //url changes all the time and needs to be corrected here when html file is opend
-    cy.visit('http://localhost:63342/PROJEKT/frontend/src/main/resources/templates/index.html?_ijt=p9tdsdrbgpi9vb4tio1ucu7fjg&_ij_reload=RELOAD_ON_SAVE')
+
+    cy.visit('http://localhost:63342/PROJEKT/frontend/src/main/resources/templates/index.html')
 
     cy.contains('Pikachu Card').click()
 
